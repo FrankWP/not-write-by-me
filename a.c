2812a4d6a41966055438c9c32f20b889e5a95bdb
soloapple <491870666@qq.com>
2016-09-15 14:11:11 +0000

    add a.c

---
+++ a.c
@@ -0,0 +1,27 @@
+/***************************************************************************************
+ *   Copyright (C), 2006-2016, Legendsec Technology Co., Ltd.
+ *
+ *    Filename: a.c
+ * Description: 
+ *     Version: 1.0
+ *     Created: Liujfa   09/15/16 14:10:53
+ *    Revision: none
+ *
+ *     History: <author>   <time>    <version >         <desc>
+ *              Liujfa   09/15/16                  build this moudle  
+ ***************************************************************************************/
+
+
+#include	<stdlib.h>
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  
+ * =====================================================================================
+ */
+	int
+main ( int argc, char *argv[] )
+{
+	return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
