d48645f8a1393e457d56c5b3789e23fadfdc3464
soloapple <491870666@qq.com>
2016-09-15 14:24:34 +0000

    master.c

---
+++ master.c
@@ -0,0 +1,28 @@
+/***************************************************************************************
+ *   Copyright (C), 2006-2016, Legendsec Technology Co., Ltd.
+ *
+ *    Filename: master.c
+ * Description: 
+ *     Version: 1.0
+ *     Created: Liujfa   09/15/16 14:24:17
+ *    Revision: none
+ *
+ *     History: <author>   <time>    <version >         <desc>
+ *              Liujfa   09/15/16                  build this moudle  
+ ***************************************************************************************/
+
+
+
+#include	<stdlib.h>
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  
+ * =====================================================================================
+ */
+	int
+main ( int argc, char *argv[] )
+{
+	return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
